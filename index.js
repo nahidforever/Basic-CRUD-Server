b4567fe8eb9d6a3848c181851eb45fd9cbc30ec2
Nahid1828 <isam15-5334@diu.edu.bd>
2026-05-11 23:18:27 +0600

    Create product by POST method also get product & single Product

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = process.env.DB_URI;
 
 const client = new MongoClient(uri, {
@@ -18,7 +22,36 @@ async function run() {
   try {
     await client.connect();
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("e-commerce");
+    const productsCollection = db.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+
+      res.send(result);
+    });
+
+    app.get("/products/:productId", async (req, res) => {
+      const productId = req.params.productId;
+
+      const query = { _id: new ObjectId(productId) };
+
+      const result = await productsCollection.findOne(query);
+
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+      console.log(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
